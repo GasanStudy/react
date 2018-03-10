4c25b0273a7a7c636e9816791d2c3c878b41e242
kwak <kwakkj112@naver.com>
2018-03-10 14:41:49 +0900

    frist commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-class App extends React.Component {
-    render(){
+    this.state = {
+      list: [],
+    };
+  }
 
-        return (
-                <h1>Hello React Skeleton</h1>
-        );
-    }
+
+  componentDidMount() {
+
+    fetch('http://localhost:8080/member/list')
+      .then(response => response.json())
+      .then(data => this.setState({ list: data }));
+
+
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>선수이름</th>
+            <th>등번호</th>
+            </tr>
+          {list.map(list =>
+            <div key={list.m_id}>
+              <tr>
+                <td>{list.name} </td>
+                <td>{list.back_num}</td>
+              </tr>
+            </div>
+          )}
+        </table>
+      </div>
+
+    )
+  }
 }
 
 export default App;
